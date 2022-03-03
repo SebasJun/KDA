054fcd37701bf1bc131f5c206abd40a43cfedfb7
SebasJun <sebastianjunhem@gmail.com>
2022-03-03 16:52:21 +0100

    Images added

--- app/src/main/java/com/example/kda/MainActivity.kt
+++ app/src/main/java/com/example/kda/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kda
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -12,5 +13,27 @@ class MainActivity : AppCompatActivity() {
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(10.toFloat())
 
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size")
+
+        val smallBtn = brushDialog.ib_small_brush
+        val mediumBtn = brushDialog.ib_medium_brush
+        val largeBtn = brushDialog.ib_large_brush
+
+        smallBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        mediumBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(20.toFloat())
+        }
+        largeBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(30.toFloat())
+        }
+        brushDialog.show()
     }
 }
